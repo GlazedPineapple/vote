c6e2189f27bc376de7330056db6ed06726649cca
GlazedPineapple <eokovacs1@gmail.com>
2021-01-01 01:03:15 -0500

    oauth login

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rocket::{launch, routes};
+use rocket::{figment::{Figment, providers::Env}, launch, routes};
 use serde::Deserialize;
 use twilight_model::id::ApplicationId;
 use twilight_oauth2::Client as OauthClient;
@@ -7,25 +7,26 @@ mod auth;
 mod routes;
 
 #[derive(Deserialize, Debug)]
-struct Config {
+pub struct Config {
     client_id: ApplicationId,
     client_secret: String,
-    // hostname: String
+    redirect_url: String
 }
 
 #[launch]
 fn rocket() -> rocket::Rocket {
     dotenv::dotenv().ok();
 
-    let config: Config = envy::from_env().unwrap();
+    let config: Config = envy::from_env().expect("Missing required environment variables");
 
     let oauth = OauthClient::new(
         config.client_id,
-        config.client_secret,
-        &["http://localhost:8080/oauth/authorize"],
-    );
+        &config.client_secret,
+        &[&config.redirect_url],
+    ).expect("Failed to create oauth client");
 
-    rocket::ignite()
+    rocket::custom(Figment::from(rocket::Config::default()).merge(Env::prefixed("ROCKET_")))
         .manage(oauth)
+        .manage(config)
         .mount("/", routes![routes::index, routes::auth::oauth_login])
 }
--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,9 +1,28 @@
-use rocket::{State, response::Redirect};
-use twilight_oauth2::Client as OauthClient;
+use rand::{thread_rng, Rng};
+use rocket::{
+    response::{Flash, Redirect},
+    State,
+};
+use twilight_oauth2::{Client as OauthClient, Prompt, Scope};
 
-#[rocket::get("/oauth/login?<from>")]
-pub fn oauth_login(from: String, oauth: State<OauthClient>) -> Redirect {
-    let auth_url = ();
+use crate::Config;
 
-    Redirect::to(/* auth_url */ "//google.com")
+const OAUTH_SCOPES: &[Scope] = &[Scope::Identify, Scope::Guilds];
+
+#[rocket::get("/oauth/login")]
+pub fn oauth_login(oauth: State<OauthClient>, config: State<Config>) -> Flash<Redirect> {
+    let state = base64::encode_config(
+        (0..32).map(|_| thread_rng().gen()).collect::<Vec<_>>(),
+        base64::URL_SAFE_NO_PAD,
+    );
+
+    let auth_url = oauth
+        .authorization_url(&config.redirect_url)
+        .expect("Redirect url is invalid")
+        .scopes(OAUTH_SCOPES)
+        .prompt(Prompt::None)
+        .state(&state)
+        .build();
+
+    Flash::new(Redirect::to(auth_url), "oauth_state", state)
 }
